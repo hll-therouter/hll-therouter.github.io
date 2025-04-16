8b28e1ffb3f0ace6a6c4e224ccaafd15832a3a3b
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2025-04-16 14:40:19 +0800

    鸿蒙文档

--- api/scripts/sourceset_dependencies.js
+++ api/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":router:dokkaJavadoccccc/androidTestRelease":[],":router:dokkaJavadoccccc/debug":[],":router:dokkaJavadoccccc/main":[],":router:dokkaJavadoccccc/release":[]}'
+sourceset_dependencies='{":router:dokkaJavadoccccc/debug":[],":router:dokkaJavadoccccc/main":[],":router:dokkaJavadoccccc/release":[]}'
